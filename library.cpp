b1c516838b4ba40761c79a50f0d3839415f109cb
davvore33 <davvore33@gmail.com>
2021-10-10 13:32:45 +0200

    Check for errors

--- library.cpp
+++ library.cpp
@@ -1,13 +1,46 @@
 #include "library.h"
 
 unsigned short Loader::load_resource(const std::string &path) {
-    return 0;
+    try {
+        boost::property_tree::ini_parser::read_ini(path, this->parsed_rs);
+    } catch (boost::property_tree::ini_parser::ini_parser_error &e) {
+        print_err(e, "read error");
+        return Loader::errors_type::READERR;
+    } catch (...) {
+        return Loader::errors_type::GENERICERROR;
+    }
+    return Loader::errors_type::OK;
 }
 
 unsigned short Loader::get_value(const std::string &key) {
-    return 0;
+    if (this->parsed_rs.empty()) {
+        return Loader::errors_type::FILENOTREADY;
+    }
+    try {
+        const std::string &ret = this->parsed_rs.get<std::string>(key);
+
+        std::cout << ret << std::endl;
+    } catch (boost::property_tree::ptree_bad_path &e) {
+        print_err(e, "bad key provided");
+        return Loader::errors_type::BADKEY;
+    } catch (...) {
+        return Loader::errors_type::GENERICERROR;
+    }
+    return Loader::errors_type::OK;
 }
 
 unsigned short Loader::set_value(const std::string &key, const std::string &value) {
-    return 0;
+    if (this->parsed_rs.empty()) {
+        return Loader::errors_type::FILENOTREADY;
+    }
+    try {
+        this->parsed_rs.put<std::string>(key, value);
+    } catch (...) {
+        return Loader::errors_type::GENERICERROR;
+    }
+    return Loader::errors_type::OK;
+}
+
+void Loader::print_err(const std::exception &e, const char *message) {
+    std::cerr << "[ERR: " << e.what() << "]: " << message << std::endl;
 }
--- library.h
+++ library.h
@@ -3,12 +3,21 @@
 
 #include <iostream>
 #define TRUE true
-
-#ifdef BOOSTFOUND
-bool boostfound = BOOSTFOUND; // https://www.boost.org/doc/libs/1_77_0/more/getting_started/unix-variants.html#get-boost
+#if BOOSTFOUND // https://www.boost.org/doc/libs/1_77_0/more/getting_started/unix-variants.html#get-boost
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/ini_parser.hpp>
 #endif
+//TODO implement a parser if you don't have boost
+
 
 class Loader {
+    enum errors_type {
+        OK = 0,
+        READERR = 1,
+        BADKEY = 3,
+        FILENOTREADY = 4,
+        GENERICERROR = 255
+    };
 public:
     Loader() {};
     ~Loader() {};
@@ -16,6 +25,10 @@ public:
     unsigned short load_resource(const std::string& path);
     unsigned short get_value(const std::string& key);
     unsigned short set_value(const std::string& key, const std::string& value);
+
+private:
+    boost::property_tree::ptree parsed_rs;
+    void print_err(const std::exception& e, const char *message);
 };
 
 #endif //EGLUE_LIB_LIBRARY_H
