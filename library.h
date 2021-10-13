ed6fc29e2160eb26523768e1fa6cce3f121be7e2
davvore33 <davvore33@gmail.com>
2021-10-14 00:38:15 +0200

    Add documentation to library code

--- library.h
+++ library.h
@@ -22,8 +22,29 @@ public:
     Loader() {};
     ~Loader() {};
 
+    /// Load and parse a .ini file
+    /// \param path Path where to load you .ini file
+    /// \return 0 : success
+    /// \return 1 : in case of read error
+    /// \return 255 : in case of generic error
     unsigned short load_resource(const std::string& path);
+
+    /// Return value of a given key from the previous loaded file
+    /// \param key Key to load
+    /// \return
+    /// \return 0 : success
+    /// \return 3 : file not loaded yet
+    /// \return 4 : key not valid
+    /// \return 255 : in case of generic error
     unsigned short get_value(const std::string& key);
+
+    /// Set the value of a give key to the previous loaded file
+    /// \param key Key to be modified
+    /// \param value New value
+    /// \return
+    /// \return 0 : success
+    /// \return 3 : file not loaded yet
+    /// \return 255 : in case of generic error
     unsigned short set_value(const std::string& key, const std::string& value);
 
 public:
